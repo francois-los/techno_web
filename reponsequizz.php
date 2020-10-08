776132b691d006fb8196a8257c48b1eb838765d2
Francois LOSFELD <32673637+francois-los@users.noreply.github.com>
2020-10-08 11:04:13 +0200

    Add response to quizz

---
+++ reponsequizz.php
@@ -0,0 +1,70 @@
+
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Reponse Quizz</title>
+  <link rel="stylesheet" href="style.css">
+  <?php include('headerr.html')?>
+  <?php include('footer.html')?>
+</head>
+
+
+
+<body>
+
+
+
+
+<section>
+  <div class="container">
+    <div class="background-img">
+      <div class="box">
+        <div class="content">
+          <h2> Réponses Quizz numéro 1 </h2>
+        </div>    
+      </div>
+    </div>
+  </div>
+</section>
+
+
+
+
+
+
+
+
+
+
+  <h3  ><br><br> Question Numéro 1 !<br><br>  </h3>
+  <p> Quelle est la date de la Révolution française ? </p>
+  <p> 18 juin 1815  </p>
+
+  <h3  ><br><br> Question Numéro 2 !<br><br>  </h3>
+  <p> Quand s'est fait le couronnement de Charlemagne ? </p>
+  <p> 25 décembre 800 </p>
+
+
+  <h3  ><br><br> Question Numéro 3 !<br><br>  </h3>
+  <p>  Quelle est la date de naissance de la reine d'Angleterre ? </p>
+  <p> 1926 </p>
+
+
+  <h3  ><br><br> Question Numéro 4 !<br><br>  </h3>
+  <p> Combien y-a-t'il de pays en Afrique ? </p>
+  <p> 54  </p>
+
+
+
+
+
+</body>
+</html>
+
+
+
+
+
+
+
---
+++ reponsequizz2.php
@@ -0,0 +1,67 @@
+
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Reponse Quizz</title>
+  <link rel="stylesheet" href="style.css">
+  <?php include('headerr.html')?>
+  <?php include('footer.html')?>
+</head>
+
+
+
+<body>
+
+
+
+
+<section>
+  <div class="container">
+    <div class="background-img">
+      <div class="box">
+        <div class="content">
+          <h2> Réponses Quizz numéro 2 </h2>
+        </div>    
+      </div>
+    </div>
+  </div>
+</section>
+
+
+
+
+
+
+
+
+
+
+  <h3  ><br><br> Question Numéro 1 !<br><br>  </h3>
+  <p> Quelle est la couleur du cheval blanc d'Henri 4 ? </p>
+  <p> Blanc </p>
+
+  <h3  ><br><br> Question Numéro 2 !<br><br>  </h3>
+  <p> Que fait une tortue le matin ? </p>
+  <p> Elle continue de dormir </p>
+
+
+  <h3  ><br><br> Question Numéro 3 !<br><br>  </h3>
+  <p>  Quand est-ce que les poules auront des dents ?</p>
+  <p> 42 </p>
+
+
+  <h3  ><br><br> Question Numéro 4 !<br><br>  </h3>
+  <p> Quelle est la réponse ? (entre 20 et 70) </p>
+  <p> 42  </p>
+
+
+
+
+
+</body>
+</html>
+
+
+
+
