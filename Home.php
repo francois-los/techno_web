289d94f51e6cce6d9b6432ffb072134e08bd83bc
RealAcharnes <jean-charles.coudon@isen.yncrea.fr>
2020-10-09 18:41:29 +0200

    home update

---
+++ Home.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="fr">
+<?php include('headerr.html')?>
+<?php include('footer.html')?>
+<head>
+  <meta charset="utf-8">
+  <title>Home</title>
+  <link rel="stylesheet" href="style.css"/>
+</head>
+
+<body>
+
+<section>
+  <div class="container">
+    <div class="background-img2">
+        <div class="box">
+        <div class="content">
+          <h1> Bienvenue </h1>
+        </div>    
+      </div>
+    </div>
+  </div>
+</section>
+
+</body>
+
+</html>
