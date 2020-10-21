67404f68d6429f5c606797abc21836dc81ccbb55
Alexis Soltysiak <65892122+nobodyonfire@users.noreply.github.com>
2020-10-21 20:33:52 +0200

    Uptade quizz 1 avec BDD

--- PHP/quiz1.php
+++ PHP/quiz1.php
@@ -26,15 +26,24 @@
 </section>
 
 
+<?php $bdd = new PDO('mysql:host=localhost;dbname=quizzsite','root',''); ?>
+
+
+
 <!-- Question 1 -->
 
 
-<form>
-  <h3  ><br><br> Question Numéro 1 !<br><br>  </h3>
-  <p> Quelle est la date de la bataille de Waterloo ?<br><br>  </p>
+
+<?php $response = $bdd->query("SELECT * FROM `question` WHERE question_id=1 " );
+
+while ($results = $response->fetch() ){
+ ?>
+  <form>
+  <h3  ><br><br> Question Numéro  <?php echo $results['question_id']; ?> !<br><br>  </h3>
+  <p> <?php echo $results['question_title']; ?><br><br>  </p>
   <div>
     <input type="radio" id="Choice1"
-     name="contact" value="1">
+     name="contact" value="1"> 
     <label for="contactChoice1">18 juin 1815</label>
 
     <input type="radio" id="Choice2"
@@ -46,27 +55,49 @@
     <label for="contactChoice3">18 juillet 1816</label>
   </div>
   <div>
-  	<h2>  </h2>
+    <h2>  </h2>
     <button type="submit">     Valider !</button>
   </div>
 </form>
+
+<?php
+}
+$response->closeCursor() ;
+?>
+
 
 
 <!-- Question 2 -->
 
 
-<h3> <br><br>Question Numéro 2 !<br><br>  </h3>
-<label >Quand s'est fait le couronnement de Charlemagne ? <br><br>
+
+<?php $response = $bdd->query("SELECT * FROM `question` WHERE question_id=2" );
+
+while ($results = $response->fetch() ){
+ ?>
+  <h3> <br><br>Question Numéro <?php echo $results['question_id']; ?> !<br><br>  </h3>
+<label ><?php echo $results['question_title']; ?><br><br>
 <input type="checkbox" name="question3" id="q3answer1" value ="answer1" checked="checked" />14 janvier 800
 <input type="checkbox" name="question3" id="q3answer2" value ="answer2" />06 septembre 800
 <input type="checkbox" name="question3" id="q3answer3" value ="answer3" />25 décembre 800</label>
 
 
+<?php
+}
+$response->closeCursor() ;
+?>
+
+
 <!-- Question 3 -->
 
 
-<h3> <br><br>Question Numéro 3 !<br><br>  </h3>
-<label for="Question-select"> Quelle est la date de naissance de la reine d'Angleterre ?</label>
+<?php $response = $bdd->query("SELECT * FROM `question` WHERE question_id=3" );
+
+while ($results = $response->fetch() ){
+ ?>
+ 
+  <h3> <br><br>Question Numéro <?php echo $results['question_id']; ?> !<br><br>  </h3>
+<label for="Question-select"><?php echo $results['question_title']; ?></label>
 <select name="Question" id="Question-select">
     <option value="">--choissiez...-</option>
     <option value="1">1924</option>
@@ -77,24 +108,52 @@
 
 </select>
 
+<?php
+}
+$response->closeCursor() ;
+?>
+
+
+
+
 
 <!-- Question 4 -->
 
 
-<h3> <br><br>Question Numéro 4 !<br><br>  </h3>
+
+<?php $response = $bdd->query("SELECT * FROM `question` WHERE question_id=4" );
+
+while ($results = $response->fetch() ){
+ ?>
+
+ <h3> <br><br>Question Numéro <?php echo $results['question_id']; ?>  !<br><br>  </h3>
 <form >
-  <label for="quantity">Combien y-a-t'il de pays en Afrique ?</label>
+  <label for="quantity"><?php echo $results['question_title']; ?></label>
   <input type="number" id="quantity" name="quantity" min="20" max="70">
   <input type="submit">
 </form>
+</select>
+
+<?php
+}
+$response->closeCursor() ;
+?>
+
+
+
 
 
 <!-- Question 5 -->
 
 
+<?php $response = $bdd->query("SELECT * FROM `question` WHERE question_id=5" );
+
+while ($results = $response->fetch() ){
+ ?>
+
 <form>
-  <h3  ><br><br> Question Numéro 5 !<br><br>  </h3>
-  <p> Quelle est la date de la bataille de Bouvines ?<br><br>  </p>
+  <h3  ><br><br> Question Numéro <?php echo $results['question_id']; ?>  !<br><br>  </h3>
+  <p> <?php echo $results['question_title']; ?><br><br>  </p>
   <div>
     <input type="radio" id="Choice1"
      name="contact" value="1">
@@ -114,22 +173,47 @@
   </div>
 </form>
 
+<?php
+}
+$response->closeCursor() ;
+?>
+
+
+
+
 
 <!-- Question 6 -->
 
+<?php $response = $bdd->query("SELECT * FROM `question` WHERE question_id=6" );
 
-<h3> <br><br>Question Numéro 6 !<br><br>  </h3>
-<label > Qui est le pharaon de Moïse ?  <br><br>
+while ($results = $response->fetch() ){
+ ?>
+
+ <h3> <br><br>Question Numéro <?php echo $results['question_id']; ?>  !<br><br>  </h3>
+<label ><?php echo $results['question_title']; ?> <br><br>
 <input type="checkbox" name="question3" id="q3answer1" value ="answer1" checked="checked" />Khéops 
 <input type="checkbox" name="question3" id="q3answer2" value ="answer2" />Ramsès II
 <input type="checkbox" name="question3" id="q3answer3" value ="answer3" />Toutânkhamon </label>
 
 
+<?php
+}
+$response->closeCursor() ;
+?>
+
+
+
+
+
 <!-- Question 7 -->
 
+<?php $response = $bdd->query("SELECT * FROM `question` WHERE question_id=7" );
 
-<h3> <br><br>Question Numéro 7 !<br><br>  </h3>
-<label for="Question-select"> Combien d'années ont duré les croisades ?</label>
+while ($results = $response->fetch() ){
+ ?>
+
+<h3> <br><br>Question Numéro <?php echo $results['question_id']; ?>  !<br><br>  </h3>
+<label for="Question-select"> <?php echo $results['question_title']; ?> </label>
 <select name="Question" id="Question-select">
     <option value="">--choissiez...-</option>
     <option value="1">100</option>
@@ -140,13 +224,26 @@
 
 </select>
 
+<?php
+}
+$response->closeCursor() ;
+?>
+
+
+
+
+
 
 <!-- Question 8 -->
 
+<?php $response = $bdd->query("SELECT * FROM `question` WHERE question_id=8" );
 
-<h3> <br><br>Question Numéro 8 !<br><br>  </h3>
+while ($results = $response->fetch() ){
+ ?>
+
+<h3> <br><br>Question Numéro <?php echo $results['question_id']; ?>  !<br><br>  </h3>
 <form >
-  <label for="quantity">Combien y-a-t'il eu de roi en France ? </label>
+  <label for="quantity"><?php echo $results['question_title']; ?> </label>
   <input type="number" id="quantity" name="quantity" min="20" max="100">
   <input type="submit">
 </form>
@@ -155,14 +252,18 @@
 
 
 
+<?php
+}
+$response->closeCursor() ;
+?>
 
-</body>
+
+
 
 
 <a href="reponsequizz.php" class="border-button"> CORRECTION </a>
 
 </html>
-
 
 
 
