2f29d62b7fa5f2b368bab928bc0f0f06bb76be35
Alexis Soltysiak <65892122+nobodyonfire@users.noreply.github.com>
2020-11-08 21:16:50 +0100

    Update headerr.php

--- PHP/headerr.php
+++ PHP/headerr.php
@@ -1,50 +1,44 @@
-<!DOCTYPE html>
-<?php include ('data.php') ?>
-<html>
-
-
-
-
 <body>
+
     <header class="bkheader">
 
             <nav class="navigation1">
                 <ul class="navigationListe">
-                    <?php
-                        if (empty($_SESSION['username'])) { ?>
-                            <br />
-                            Please connect to access quizzes
-                        <?php } else { ?>
-                            <li><a href="./index.php?p=quizz" title="Go to Quiz 1" >Quizz 1</a></li>
-                            <li><a href="./index.php?p=quizz" title="Go to Quiz 2">Quizz 2</a></li>
-                        <?php } ?>
+                	<?php
+                		if (empty($_SESSION['username'])) { ?>
+                			<br />
+                			Please connect to access quizzes
+                		<?php } else { ?>
+                            <?php foreach ($quizzidarray as $quizzKey => $quizzValue) {?>
+                			<li><a href="../PHP/index.php?direction=quiz&id=<?php echo $quizzValue ?>" title="Go to Quiz <?php echo $quizzValue ?>">Quizz <?php echo $quizzValue ?></a></li>
+                    		<?php }?>
+                    	<?php } ?>
                 </ul>
             </nav>
-
+            
             <nav class="navigation2">
                 <ul class="navigationListe">
-                    <li><a href="./index.php?p=home" title="Go to home">Home</a></li>
+                    <li><a href="index.php?direction=home" title="Go to home">Home</a></li>
                 </ul>
             </nav>
             <?php
-                if (empty($_SESSION['username'])) { ?>
-                    <form method="post" class="loginForm" action="./?p=home">
-                        <label for="identifiant"> Username :    </label>
-                        <input type="text" name="username" id="UsernameId" required/>
-                        <br />
-                        <label for="password"> Password : </label>
-                        <input type="password" name="password" id="passwordId" required/>
-                        <br />
-                        <button type="submit" name='login_user'>Validate</button>
-                        <input type="button" value="Create account" onclick=self.location.href="./?p=create">
-                    </form>
-                   <?php } else { ?>
-                       <a href="./?logout=1,p=home" class = "logout">Logout</a>
-                   <?php } ?>
-                   <?php if (isset($error1)){?>
+            	if (empty($_SESSION['username'])) { ?>
+		            <form method="post" class="loginForm" action="index.php?direction=home">
+		                <label for="identifiant"> Username :	</label>
+		                <input type="text" name="username" id="UsernameId" required/>
+		                <br />
+		                <label for="password"> Password : </label>
+		                <input type="password" name="password" id="passwordId" required/>
+		                <br />
+		                <button type="submit" name='login_user'>Validate</button>
+		                <input type="button" value="Create account" onclick=self.location.href="index.php?direction=createAccount">
+		            </form>
+		       	<?php } else { ?>
+		       		<a href="index.php?direction=home&logout=1" class = "logout">Logout</a>
+		       	<?php } ?>
+		       	<?php if (isset($error1)){?>
                     <span class = error1> Wrong username or password </span>
                 <?php } ?>
-
+        
     </header>
 </body>
-</html>
