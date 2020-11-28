81a1ec6331942125a876454e4fc146dbb22cb102
RealAcharnes <65890983+RealAcharnes@users.noreply.github.com>
2020-11-28 23:55:43 +0100

    Update resumequizz.php

--- PHP/resumequizz.php
+++ PHP/resumequizz.php
@@ -14,7 +14,7 @@ $_SESSION['quizzid'] = $quizzid;
 
 	<?php
 		$resumearray = array();
-		$request = $bdd->prepare("SELECT user.username, score.score FROM score INNER JOIN user ON score.score_user_id = user.user_id WHERE score_quizz_id=? ORDER BY score DESC");
+		$request = $bdd->prepare("SELECT user.username, score.score, score.timeQuizz FROM score INNER JOIN user ON score.score_user_id = user.user_id WHERE score_quizz_id=? ORDER BY score DESC, timeQuizz");
 		$request->execute([$quizzid]);
 		while ($results=$request->fetch()){
 			$resumearray[] = $results;
@@ -39,17 +39,30 @@ $_SESSION['quizzid'] = $quizzid;
 		<tr>
 			<th class="thgen">User</th>
 			<th class="thgen">Score out of 10</th>
+			<th class="thgen">Time</th>
 		</tr>
 
 		<?php
-			foreach ($resumearray as $key => $value) { ?>
+
+			foreach ($resumearray as $key => $value) {
+
+				if ($value['timeQuizz'] >= 60){
+					$minutes = floor($value['timeQuizz'] / 60);
+					$secondes = $value['timeQuizz'] - ($minutes * 60);
+				}
+				else {
+					$secondes = $value['timeQuizz'];
+					$minutes = 0;
+				}
+				$formatedTime = "$minutes min $secondes s";?>
 				<tr>
 		
 					<td class="tdgen "><?php echo $value['username'] ?></td>
 					<td class="tdgen "><?php echo $value['score'] ?></td>
+					<td class="tdgen "><?php echo $formatedTime ?></td>
 			
 				</tr>
-			<?php } ?>
+		<?php } ?>
 
 	</table>
 
